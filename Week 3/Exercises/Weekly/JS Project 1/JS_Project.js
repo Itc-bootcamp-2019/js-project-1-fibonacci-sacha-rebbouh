4d61e1452ce090677247837fa05a685d630d4a53
Sacha Rebbouh <sacha.rebbouh@gmail.com>
2020-03-04 20:13:11 +0200

    Milestone 5

--- Week 3/Exercises/Weekly/JS Project 1/JS_Project.js
+++ Week 3/Exercises/Weekly/JS Project 1/JS_Project.js
@@ -1,28 +1,31 @@
+// document.getElementById("firstNumber").innerText = "X";
+// document.getElementById("secondNumber").innerText = "Y";
+
 function fib(n) {
-	firstNum = 1;
-	secondNum = 0;
-	sumNum = firstNum + secondNum;
-	for (i = 0; i < n; i += 1) {
-		sumNum = firstNum + secondNum;
+	let firstNum = 1;
+	let secondNum = 0;
+	let sumNum;
+	for (let i = 0; i <= n; i += 1) {
+		let sumNum = firstNum + secondNum;
 		firstNum = secondNum;
 		secondNum = sumNum;
 	}
 	return sumNum;
 }
 
-let n;
-sumNum = fib(n);
-
-// document.getElementById("firstNumber").innerText = n;
-// document.getElementById("secondNumber").innerText = sumNum;
+// let n;
+// let n;
+// let m = fib(n);
 
 executeFib.addEventListener("click", () => {
 	let a = firstNumber.value;
-	let b = fib(a);
-	document.getElementById("secondNumber").innerText = b;
-});
+	// let b = fib(a);
+	// document.getElementById("secondNumber").innerText = b;
 
-// function fibRec(n) {
-// 	if (n <= 1) return n;
-// 	return fib(n - 2) + fib(n - 1);
-// }
+	let url = "http://localhost:5050/fibonacci/" + a;
+	fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			document.getElementById("secondNumber").innerText = data.result;
+		});
+});
