c061cd7071c3fa8abca1a2e59b6465f36e32cfda
Sacha Rebbouh <sacha.rebbouh@gmail.com>
2020-03-14 20:23:15 +0200

    Milestone 8

--- Week 3/Exercises/Weekly/JS Project 1/JS_Project.js
+++ Week 3/Exercises/Weekly/JS Project 1/JS_Project.js
@@ -1,3 +1,20 @@
+let executeFib = document.getElementById("executeFib");
+let fiboLoader = document.getElementById("fiboLoader");
+let redBox = document.getElementById("redBox");
+let checkbox = document.getElementById("checkbox");
+
+function fib(n) {
+	let firstNum = 1;
+	let secondNum = 0;
+	let sumNum;
+	for (let i = 0; i <= n; i += 1) {
+		let sumNum = firstNum + secondNum;
+		firstNum = secondNum;
+		secondNum = sumNum;
+	}
+	return sumNum;
+}
+
 function isValidJSON(str) {
 	try {
 		JSON.parse(str);
@@ -6,59 +23,45 @@ function isValidJSON(str) {
 	}
 	return true;
 }
-// document.getElementById("firstNumber").innerText = "X";
-// document.getElementById("secondNumber").innerText = "Y";
-
-// function fib(n) {
-// 	let firstNum = 1;
-// 	let secondNum = 0;
-// 	let sumNum;
-// 	for (let i = 0; i <= n; i += 1) {
-// 		let sumNum = firstNum + secondNum;
-// 		firstNum = secondNum;
-// 		secondNum = sumNum;
-// 	}
-// 	return sumNum;
-// }
-
-// let n;
-// let n;
-// let m = fib(n);
 
 function executeAll() {
-	let executeFib = document.getElementById("executeFib");
-
-	executeFib.addEventListener("click", () => {
-		let a = firstNumber.value;
-		let secondNumber = document.getElementById("secondNumber");
-		let fiboLoader = document.getElementById("fiboLoader");
-		let redBox = document.getElementById("redBox");
-
-		if (a > 50) {
-			firstNumber.classList.add("over-fifty");
-			redBox.classList.add("light-pink");
-			redBox.innerText = "Can't be larger than 50";
-		} else {
-			fiboLoader.style.display = "block";
-			// let b = fib(a);
-			// secondNumber.innerText = b;
-			let url = "http://localhost:5050/fibonacci/" + a; //template string
-			fetch(url)
-				.then(res => res.text())
-				.then(data => {
-					fiboLoader.style.display = "none";
-					if (isValidJSON(data)) {
-						let isJSON = JSON.parse(data);
-						secondNumber.innerText = isJSON.result;
-					} else {
-						let servError = "Server Error: ";
-						let globalMessage = servError + data;
-						secondNumber.innerText = globalMessage;
-					}
-					secondNumber.style.display = "block";
-				});
-		}
-	});
+	let a = firstNumber.value;
+	let secondNumber = document.getElementById("secondNumber");
+	if (checkbox.checked == true) {
+		console.log(checkbox.checked);
+		executeFib.addEventListener("click", () => {
+			if (a > 50) {
+				firstNumber.classList.add("over-fifty");
+				redBox.classList.add("light-pink");
+				redBox.innerText = "Can't be larger than 50";
+			} else {
+				fiboLoader.style.display = "block";
+				// let b = fib(a);
+				// secondNumber.innerText = b;
+				let url = "http://localhost:5050/fibonacci/" + a; //template string
+				fetch(url)
+					.then(res => res.text())
+					.then(data => {
+						fiboLoader.style.display = "none";
+						if (isValidJSON(data)) {
+							let isJSON = JSON.parse(data);
+							secondNumber.innerText = isJSON.result;
+						} else {
+							let servError = "Server Error: ";
+							let globalMessage = servError + data;
+							secondNumber.innerText = globalMessage;
+						}
+						secondNumber.style.display = "block";
+					});
+			}
+		});
+		// } else {
+		// 	let a = firstNumber.value;
+		// 	let secNb = fib(a);
+		// 	// console.log(fib(a));
+		// 	secondNumber.innerText = secNb;
+		// }
+	}
 }
 
 function resultList() {
@@ -73,7 +76,7 @@ function resultList() {
 					let b = data.results[i];
 					let searchDate = data.results[i].createdDate;
 					let fullDate = new Date(searchDate);
-					console.log(data.results.length);
+					// console.log(data.results.length);
 
 					results.innerHTML +=
 						"The Fibonnaci of " +
